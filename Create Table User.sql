5bcb6802c23ca9640a07154ef2827f0e6e8f7b5c
Jamaludin <61225319+Jamaludin078@users.noreply.github.com>
2024-08-03 23:03:04 +0700

    script
    script

---
+++ Create Table User.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[User](
+	[UserId] [varchar](10) NOT NULL,
+	[FirstName] [varchar](50) NULL,
+	[LastName] [varchar](50) NULL,
+	[Email] [varchar](50) NULL,
+	[TNC] [bit] NOT NULL,
+	[Password] [varchar](50) NULL,
+	[RegisterDate] [datetime] NULL,
+	[MemberId] [int] NULL,
+	[CreateDate] [datetime] NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
